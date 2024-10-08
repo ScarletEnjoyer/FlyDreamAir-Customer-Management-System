6a99b67dcedfea34227cec19e88fb3ef75b675af
ScarletEnjoyer <2218039670@qq.com>
2024-10-08 12:30:34 +1100

    main
    main code

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from storage.userStorage import init_user_storage
+from ui.mainWindow import MainWindow
+
+
+if __name__ == '__main__':
+    init_user_storage()
+
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
